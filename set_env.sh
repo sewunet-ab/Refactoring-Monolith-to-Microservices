09f42ee19a3fb73abca05969b0e62c529cc0da06
sewunet_ab <sewunetabebaw92@gmail.com>
2022-10-01 21:19:25 +0300

    add github actions to my repo

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=my-weak-password
+export POSTGRES_HOST=project-3-db.czrmvufll8fb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=udagram-sewunet-project
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
